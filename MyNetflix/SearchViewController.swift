c2158cc73c5513e6ed9cb65bfdc9b921a5931305
jeje.e <jeje.e@kakaocorp.com>
2021-07-11 16:04:43 +0900

    init

---
+++ MyNetflix/SearchViewController.swift
@@ -0,0 +1,109 @@
+
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    @IBOutlet weak var resultCollectionView: UICollectionView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    
+    func dismissKeyboard() {
+        searchBar.resignFirstResponder()
+    }
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        dismissKeyboard()
+        
+        guard let searchTerm = searchBar.text, searchTerm.isEmpty == false else { return }
+        
+        SearchAPI.search(searchTerm, completion:{ movies in
+            
+            
+        })
+    }
+}
+
+class SearchAPI {
+    static func search(_ term: String, completion: @escaping ([Movie]) -> Void) {
+        let session = URLSession(configuration: .default)
+        
+        var urlComponents = URLComponents(string: "https://itunes.apple.com/search?")!
+        let mediaQuery = URLQueryItem(name: "media", value: "movie")
+        let entityQuery = URLQueryItem(name: "entity", value: "movie")
+        let termQuery = URLQueryItem(name: "term", value: term)
+        urlComponents.queryItems?.append(mediaQuery)
+        urlComponents.queryItems?.append(entityQuery)
+        urlComponents.queryItems?.append(termQuery)
+        
+        let requestURL = urlComponents.url!
+        
+        let dataTask = session.dataTask(with: requestURL, completionHandler: {
+            data, response, error in
+            
+            let successRange = 200..<300
+            
+            guard error == nil,
+                  let statusCode = (response as? HTTPURLResponse)?.statusCode, successRange.contains(statusCode)
+            else {
+                completion([])
+                return
+            }
+            
+            guard let resultData = data else {
+                completion([])
+                return
+            }
+            
+            let movies = SearchAPI.parseMovies(resultData)
+            completion(movies)
+            
+        })
+        dataTask.resume()
+        
+    }
+    
+    static func parseMovies(_ data: Data) -> [Movie]{
+        let decoder = JSONDecoder()
+        
+        do{
+            let response = try decoder.decode(Response.self, from: data)
+            let movies = response.movies
+            return movies
+        }catch let error{
+            print("---- error \(error.localizedDescription)")
+            return []
+        }
+    }
+}
+
+
+struct Response: Codable {
+    let resultCount: Int
+    let movies: [Movie]
+    
+    enum CodingKeys: String, CodingKey {
+        case resultCount
+        case movies = "results"
+    }
+}
+
+struct Movie: Codable {
+    let title: String
+    let director: String
+    let thumbnailPath: String
+    let previewURL: String
+    
+    enum CodingKeys: String, CodingKey {
+        case title = "trackName"
+        case director = "artistName"
+        case thumbnailPath = "artworkUrl100"
+        case previewURL = "previewUrl"
+    }
+}
